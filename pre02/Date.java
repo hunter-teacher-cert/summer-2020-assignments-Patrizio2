6c9c65440d83cbec6e94a5fda1cfcf2811dfe784
Patrizio2 <67011697+Patrizio2@users.noreply.github.com>
2020-06-23 23:30:47 -0400

    yeah haw, finally got the hang of this

---
+++ pre02/Date.java
@@ -0,0 +1,29 @@
+public class Date {
+
+    public static void main(String[] args) {
+
+      String   day = "Tuesday";
+      String month = "June";
+      String  year = "2020";
+      String  date = "23";
+
+//prints American format
+        System.out.println("American Format:");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+//prints European Format
+        System.out.println("European Format:");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.println(year);
+      }
+}
