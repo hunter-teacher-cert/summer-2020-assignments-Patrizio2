b28daf60b06dea30cc7e90ecb4b297cd6825356b
Patrizio2 <67011697+Patrizio2@users.noreply.github.com>
2020-07-02 13:32:56 -0400

    updated time printed

--- Classwork/Driver.java
+++ Classwork/Driver.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Driver{
-    public static void main(String[] args) {
-	Time t = new Time();
-    }
+  public static void main(String[] args) {
+      Time time = new Time(2, 59, 59.9);
+      System.out.println(time);
+  }
 }
