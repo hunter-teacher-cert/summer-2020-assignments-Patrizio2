87eaf505069ce7920df0a0ba966b20536637e60a
Patrizio2 <67011697+Patrizio2@users.noreply.github.com>
2020-07-05 21:30:56 -0400

    got the fine tuning...just need to complete

--- Classwork/day1.java
+++ Classwork/day1.java
@@ -11,6 +11,7 @@ public class day1 {
     int colreplace;
     int value;
     int choice;
+    int direction;
     Scanner sc = new Scanner(System.in);
 
     int[][] grid = new int[rows][cols];
@@ -24,7 +25,7 @@ public class day1 {
     System.out.println("What would you like to do:");
     System.out.println("1) Replace a row");
     System.out.println("2) Replace a column");
-    System.out.println("3) Invert the gride");
+    System.out.println("3) Invert the grid");
     System.out.println("4) Create a diagonal");
     System.out.println("5) Print the grid");
 //    System.out.println("4) Create a diagonal");
@@ -53,7 +54,20 @@ public class day1 {
       invert(grid);
       print2d(grid);
   } else if (choice ==4){
-
+    System.out.println("What row?");
+    // prompt the user and get the value
+    rowreplace = sc.nextInt();
+    System.out.println("What column?");
+    // prompt the user and gets the row to replace
+    colreplace = sc.nextInt();
+    System.out.println("What direction?");
+    // prompt the user and get the value
+    direction = sc.nextInt();
+    System.out.println("What value would you like to use?");
+    // prompt the user and get the value
+    value = sc.nextInt();
+    diagonal(grid, rowreplace, colreplace, direction, value);
+    print2d(grid);
   } else if (choice == 5){
     print2d(grid);
   }else if (choice == 6){
@@ -107,12 +121,10 @@ public class day1 {
         }else{
             d2[r][c] = 0;
         }
-
       }//end for c loop
-
     }//end for r loop
-
   }//end invert
+
 
   /*
     0: up + left
@@ -121,6 +133,42 @@ public class day1 {
     3: down + right
    */
   public static void diagonal(int[][] d2, int r, int c, int direction, int value) {
+    int columncount = (c-1);
+    if ( direction == 3){
+    for (int startrow = r ; startrow < d2.length ; startrow++){
+        if (columncount < d2[startrow].length){
+        d2[startrow][columncount] = value;
+        System.out.print("David Byrne");
+        columncount ++ ;
+      }else{
+            break;
+          }
+//      for (int startcolumn = c ; startcolumn < d2[r].length ; startcolumn++){
+//        }
+      }
+
+  }
+//    if (direction == 0 || 1 ){
+//        rowadjust = -1;
+//        if (direction == 0){
+//            columnadjust = -1;
+//       else
+//            columnadjust = 1;
+
+//          }
+
+//   }else if (direction == 2){
+//         rowadjust = 1;
+//         columnadjust = -1;
+//   }else{
+//          rowadjust = 1;
+//          columnadjust = 1;
+//        }
+//      }
+
+//      for (int row = r ; r < d2.length ; row+= rowadjust){
+//        for (int column = c ; d2[r].length ; column+= columnadjust  )
+//      }
   }//end diagonal
 
-}
+}//end day1
