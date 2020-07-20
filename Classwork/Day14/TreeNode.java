7a4a847ad8b212676c26b938261f75f6208081d7
Patrizio2 <67011697+Patrizio2@users.noreply.github.com>
2020-07-20 11:45:04 -0400

    starting,good so far

---
+++ Classwork/Day14/TreeNode.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class TreeNode{
+  private int data;
+  private TreeNode left,right;
+
+  public TreeNode(){
+    this(0);
+    left = null;
+    right = null;
+  }
+
+  public TreeNode(int data){
+    this.data = data;
+    left = null;
+    right = null;
+  }
+
+
+
+
+}
